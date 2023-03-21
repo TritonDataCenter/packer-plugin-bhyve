a8fa90e46696345a9879eaa27ff6c389891475b2
Jonathan Perkin <jonathan@perkin.org.uk>
2023-03-21 16:04:30 +0000

    Improve reliability of "zfs destroy".
    It seems to take a while after destroying the bhyve VM for "zfs destroy"
    to not return EBUSY on the volume in use, so retry a few times.
    
    While here improve error messages, as they were useful during debugging.

--- builder/bhyve/step_create_zvol.go
+++ builder/bhyve/step_create_zvol.go
@@ -1,10 +1,13 @@
 package bhyve
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
@@ -29,8 +32,12 @@ func (step *stepCreateZvol) Run(ctx context.Context, state multistep.StateBag) m
 	ui.Say(fmt.Sprintf("Creating ZFS zvol %s", zvol_path))
 
 	cmd := exec.Command("/usr/sbin/zfs", args...)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
 	if err := cmd.Run(); err != nil {
-		err = fmt.Errorf("Error creating zvol: %s", err)
+		err := fmt.Errorf("Error creating zvol: %s", strings.TrimSpace(stderr.String()))
+		state.Put("error", err)
+		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
 
@@ -50,8 +57,21 @@ func (step *stepCreateZvol) Cleanup(state multistep.StateBag) {
 
 	ui.Say(fmt.Sprintf("Destroying ZFS zvol %s", zvol_path))
 
-	cmd := exec.Command("/usr/sbin/zfs", args...)
-	if err := cmd.Run(); err != nil {
-		log.Fatal("Error destroying zvol: %s", err)
+	// Despite bhyvectl --destroy running before us, this will often fail
+	// with EBUSY for a few seconds afterwards, so we retry a few times.
+	var retries = 4
+	for i := 1; i <= retries; i++ {
+		cmd := exec.Command("/usr/sbin/zfs", args...)
+		var stderr bytes.Buffer
+		cmd.Stderr = &stderr
+		if err := cmd.Run(); err != nil {
+			if i == retries {
+				log.Printf("Error destroying zvol: %s", strings.TrimSpace(stderr.String()))
+				break
+			}
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		break
 	}
 }
